678ba091969fccad7ad8862acf1cd782eaebeb82
ArthurAraujo05 <112352001+ArthurAraujo05@users.noreply.github.com>
2024-08-13 20:27:14 -0300

    Instalação do projeto+ biblioteca

---
+++ quadro/app/achivements/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+export default function achivements() {
+  return (
+    <>
+      <h1> achivements </h1>
+    </>
+  )
+}
---
+++ quadro/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+export default function dashboard() {
+  return (
+    <>
+      <h1> dashboard </h1>
+    </>
+  )
+}
