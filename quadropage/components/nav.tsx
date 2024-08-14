afaa3e91132295577b7991aa00982801ebf5ecdb
geovaniorsoli <112351284+geovaniorsoli@users.noreply.github.com>
2024-08-14 12:37:58 -0300

    mobile responsividade

--- quadropage/components/nav.tsx
+++ quadropage/components/nav.tsx
@@ -3,50 +3,119 @@ import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuIte
 import { Button } from "@/components/ui/button"
 import * as lucide from "lucide-react"
 import { useTheme } from "next-themes"
+import { useState } from "react"
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
+
 
 export default function Nav() {
-    const { setTheme } = useTheme()
+    const { theme, setTheme } = useTheme()
+    const [showThemeMobile, setShowThemeMobile] = useState(" ")
+    const [isOpen, setIsOpen] = useState(false)
+
+    const themeMobile = () => {
+        setTheme(theme === "light" ? "dark" : "light")
+    }
+
 
     return (
         <header className="flex h-16 w-full items-center justify-between bg-background px-4 md:px-6">
-            <Link href="/" className="flex items-center" prefetch={false}>
-                <img className="h-6 w-6" />
-                <span className="ml-2 text-lg font-medium">Quadro.</span>
-            </Link>
-            <nav className="flex items-center space-x-4">
-                <Link href="/" className="text-sm font-medium hover:underline" prefetch={false}>
-                    Home
+            <div className="flex items-center space-x-4">
+                <Link href="/" className="flex items-center" prefetch={false}>
+                    <img className="h-6 w-6" />
+                    <span className="ml-2 text-lg font-medium">Quadro.</span>
                 </Link>
-                <Link href="#" className="text-sm font-medium hover:underline" prefetch={false}>
-                    Sobre
-                </Link>
-                <Button asChild variant="outline">
+                <nav className="hidden md:flex items-center space-x-4">
+                    <Link href="/" className="text-sm font-medium hover:underline" prefetch={false}>
+                        Home
+                    </Link>
+                    <Link href="#" className="text-sm font-medium hover:underline" prefetch={false}>
+                        Sobre
+                    </Link>
+                </nav>
+            </div>
+            <div className="flex items-center space-x-2">
+                <Button className="hidden md:block" asChild variant="outline">
                     <Link href="/join">
                         Entrar
-                    </Link> 
+                    </Link>
                 </Button>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" size="icon" className="rounded-full">
-                            <lucide.SunMoon className="h-5 w-5" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuItem onClick={() => setTheme("light")}>
-                            <div className="flex items-center justify-between gap-2">
-                                <lucide.Sun className="h-4 w-4" />
-                                <span> Claro </span>
-                            </div>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem onClick={() => setTheme("dark")}>
-                            <div className="flex items-center justify-between gap-2">
-                                <lucide.Moon className="h-4 w-4" />
-                                <span> Escuro </span>
-                            </div>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </nav>
+                <div className="hidden md:block">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="secondary" size="icon" className="rounded-full">
+                                <lucide.SunMoon className="h-5 w-5" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem onClick={() => setTheme("light")}>
+                                <div className="flex items-center justify-between gap-2">
+                                    <lucide.Sun className="h-4 w-4" />
+                                    <span> Claro </span>
+                                </div>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setTheme("dark")}>
+                                <div className="flex items-center justify-between gap-2">
+                                    <lucide.Moon className="h-4 w-4" />
+                                    <span> Escuro </span>
+                                </div>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+            </div>
+            <Drawer>
+                <DrawerTrigger className="md:hidden"><lucide.Menu /></DrawerTrigger>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>Para onde gostaria de ir?</DrawerTitle>
+                        <DrawerDescription>rotas</DrawerDescription>
+                    </DrawerHeader>
+
+                    <DrawerFooter className="gap-3">
+                        <div >
+                            <Button className="w-full mb-3" asChild>
+                                <Link href="/join">
+                                    Entrar
+                                </Link>
+                            </Button>
+                            <Button className="w-full mb-3" asChild variant="outline">
+                                <Link href="/">
+                                    Home
+                                </Link>
+                            </Button>
+                            <Button className="w-full mb-3" asChild variant="outline">
+                                <Link href="/">
+                                    Sobre
+                                </Link>
+                            </Button>
+                            <Button className="w-full mb-3" asChild variant="outline">
+                                <Link href="/join">
+                                    Contato
+                                </Link>
+                            </Button>
+
+                            <Button className="w-full mb-3" onClick={themeMobile} variant="secondary">
+                                Tema
+                            </Button>
+                        </div>
+                    </DrawerFooter>
+                    <DrawerFooter>
+                        <DrawerClose>
+                            <Button variant="ghost" className="w-full">fechar</Button>
+                        </DrawerClose>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+
         </header>
     )
 }
