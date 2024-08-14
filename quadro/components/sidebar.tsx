991c3878dfe6147a82c26c3bac1b7528c351021f
ArthurAraujo05 <112352001+ArthurAraujo05@users.noreply.github.com>
2024-08-14 19:16:17 -0300

    Update sidebar.tsx

--- quadro/components/sidebar.tsx
+++ quadro/components/sidebar.tsx
@@ -1,25 +1,29 @@
-"use client"
+"use client";
 
-import React from "react"
-import { Avatar, AvatarImage, AvatarFallback } from './ui/avatar'
-import { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from './ui/tooltip'
-import { Button } from './ui/button'
-import * as lucide from 'lucide-react'
-import Link from "next/link"
+import { usePathname } from 'next/navigation';
+import { Avatar, AvatarImage, AvatarFallback } from './ui/avatar';
+import { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from './ui/tooltip';
+import { Button } from './ui/button';
+import * as lucide from 'lucide-react';
+import Link from 'next/link';
 
 export default function Sidebar() {
+    const pathname = usePathname();
+    const isDisable = (route: string) => pathname === route;
 
     return (
         <div className="fixed top-0 left-0 h-screen border-r z-50">
             <div className="flex flex-col flex-initial h-screen border-r p-4 gap-y-32">
                 <TooltipProvider delayDuration={300}>
                     <Tooltip>
-                        <Link href="/home">
-                            <Avatar className="w-10 h-10">
-                                <AvatarImage src="/placeholder-user.jpg" alt="@shadcn" />
-                                <AvatarFallback></AvatarFallback>
-                            </Avatar>
-                        </Link>
+                        <TooltipTrigger asChild>
+                            <Link href="/home">
+                                <Avatar className="w-10 h-10">
+                                    <AvatarImage src="/placeholder-user.jpg" alt="@shadcn" />
+                                    <AvatarFallback></AvatarFallback>
+                                </Avatar>
+                            </Link>
+                        </TooltipTrigger>
                     </Tooltip>
                 </TooltipProvider>
                 <div className="flex flex-col items-start justify-start flex-1 gap-4">
@@ -27,14 +31,15 @@ export default function Sidebar() {
                         <Tooltip>
                             <TooltipTrigger asChild>
                                 <span>
-                                    <Button
-                                        variant="ghost"
-                                        size="icon"
-                                    >
-                                        <Link href="/statement" className="flex items-center">
+                                    <Link href="/statement" className="flex items-center">
+                                        <Button
+                                            variant="ghost"
+                                            size="icon"
+                                            disabled={isDisable("/statement")}
+                                        >
                                             <lucide.Coins className="w-6 h-6" />
-                                        </Link>
-                                    </Button>
+                                        </Button>
+                                    </Link>
                                 </span>
                             </TooltipTrigger>
                             <TooltipContent side="right">Extrato</TooltipContent>
@@ -42,14 +47,15 @@ export default function Sidebar() {
                         <Tooltip>
                             <TooltipTrigger asChild>
                                 <span>
-                                    <Button
-                                        variant="ghost"
-                                        size="icon"
-                                    >
-                                        <Link href="/achievement" className="flex items-center">
+                                    <Link href="/achievement" className="flex items-center">
+                                        <Button
+                                            variant="ghost"
+                                            size="icon"
+                                            disabled={isDisable("/achievement")}
+                                        >
                                             <lucide.PiggyBank className="w-6 h-6" />
-                                        </Link>
-                                    </Button>
+                                        </Button>
+                                    </Link>
                                 </span>
                             </TooltipTrigger>
                             <TooltipContent side="right">Metas</TooltipContent>
@@ -57,14 +63,15 @@ export default function Sidebar() {
                         <Tooltip>
                             <TooltipTrigger asChild>
                                 <span>
-                                    <Button
-                                        variant="ghost"
-                                        size="icon"
-                                    >
-                                        <Link href="/dashboard" className="flex items-center">
+                                    <Link href="/dashboard" className="flex items-center">
+                                        <Button
+                                            variant="ghost"
+                                            size="icon"
+                                            disabled={isDisable("/dashboard")}
+                                        >
                                             <lucide.ChartArea className="w-6 h-6" />
-                                        </Link>
-                                    </Button>
+                                        </Button>
+                                    </Link>
                                 </span>
                             </TooltipTrigger>
                             <TooltipContent side="right">Resumo</TooltipContent>
@@ -76,14 +83,15 @@ export default function Sidebar() {
                         <Tooltip>
                             <TooltipTrigger asChild>
                                 <span>
-                                    <Button
-                                        variant="ghost"
-                                        size="icon"
-                                    >
-                                        <Link href="/settings" className="flex items-center">
+                                    <Link href="/settings" className="flex items-center">
+                                        <Button
+                                            variant="ghost"
+                                            size="icon"
+                                            disabled={isDisable("/settings")}
+                                        >
                                             <lucide.Settings className="w-6 h-6" />
-                                        </Link>
-                                    </Button>
+                                        </Button>
+                                    </Link>
                                 </span>
                             </TooltipTrigger>
                             <TooltipContent side="right">Configurações</TooltipContent>
@@ -92,5 +100,5 @@ export default function Sidebar() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
