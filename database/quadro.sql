adcaa3a3203abf1b6394b89561529e093376d8a2
Moyseys <moyseys.veroni@gmail.com>
2024-08-27 20:58:00 -0300

    Update table names

--- database/quadro.sql
+++ database/quadro.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `quadro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `quadro`;
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: quadro
 -- ------------------------------------------------------
--- Server version	8.0.39
+-- Server version	8.2.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,21 +18,30 @@ USE `quadro`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `balances`
+-- Table structure for table `banlance`
 --
 
-DROP TABLE IF EXISTS `balances`;
+DROP TABLE IF EXISTS `banlance`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `balances` (
+CREATE TABLE `banlance` (
   `id` int NOT NULL AUTO_INCREMENT,
   `value` decimal(15,4) DEFAULT NULL,
   `team_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_team` (`team_id`),
-  CONSTRAINT `balances_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `banlance_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `banlance`
+--
+
+LOCK TABLES `banlance` WRITE;
+/*!40000 ALTER TABLE `banlance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `banlance` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `fixed_income`
@@ -49,18 +58,27 @@ CREATE TABLE `fixed_income` (
   `user_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `fixed_income_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `fixed_income_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `goals`
+-- Dumping data for table `fixed_income`
 --
 
-DROP TABLE IF EXISTS `goals`;
+LOCK TABLES `fixed_income` WRITE;
+/*!40000 ALTER TABLE `fixed_income` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fixed_income` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `goal`
+--
+
+DROP TABLE IF EXISTS `goal`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `goals` (
+CREATE TABLE `goal` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) DEFAULT NULL,
   `description` varchar(500) DEFAULT NULL,
@@ -68,37 +86,55 @@ CREATE TABLE `goals` (
   `team_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `team_id` (`team_id`),
-  CONSTRAINT `goals_ibfk_1` FOREIGN KEY (`id`) REFERENCES `teams` (`id`),
-  CONSTRAINT `team_id` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`)
+  CONSTRAINT `goal_ibfk_1` FOREIGN KEY (`id`) REFERENCES `team` (`id`),
+  CONSTRAINT `team_id` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `tags`
+-- Dumping data for table `goal`
 --
 
-DROP TABLE IF EXISTS `tags`;
+LOCK TABLES `goal` WRITE;
+/*!40000 ALTER TABLE `goal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `goal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tags` (
+CREATE TABLE `tag` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `theme` varchar(255) DEFAULT NULL,
   `balance_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `balance_id` (`balance_id`),
-  CONSTRAINT `tags_ibfk_1` FOREIGN KEY (`balance_id`) REFERENCES `balances` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`balance_id`) REFERENCES `banlance` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `teams`
+-- Dumping data for table `tag`
 --
 
-DROP TABLE IF EXISTS `teams`;
+LOCK TABLES `tag` WRITE;
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `team`
+--
+
+DROP TABLE IF EXISTS `team`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `teams` (
+CREATE TABLE `team` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) DEFAULT NULL,
   `description` varchar(500) DEFAULT NULL,
@@ -107,13 +143,22 @@ CREATE TABLE `teams` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `users`
+-- Dumping data for table `team`
 --
 
-DROP TABLE IF EXISTS `users`;
+LOCK TABLES `team` WRITE;
+/*!40000 ALTER TABLE `team` DISABLE KEYS */;
+/*!40000 ALTER TABLE `team` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
+CREATE TABLE `user` (
   `id_user` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
@@ -123,6 +168,15 @@ CREATE TABLE `users` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -133,4 +187,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-21 20:38:38
+-- Dump completed on 2024-08-27 20:57:28
